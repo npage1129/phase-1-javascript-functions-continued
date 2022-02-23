3a251ba659e2397ff9de840f0c813c0471af3179
nicolepage <npage1129@gmail.com>
2022-02-23 15:35:05 -0700

    commit tree c484197f8855f127f6bba8e3e64bd52064a48166

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("swim")
+
+const mondayWork = function(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`);
+};
+mondayWork();
+
